1efdbd1044e738e49240668cb582856633d043ad
Clarence Nguyen <clarrynguyen@gmail.com>
2015-10-03 18:01:07 -0700

    fixed

--- app/src/main/java/com/example/clarencenguyen/liftr/LiftrApplication.java
+++ app/src/main/java/com/example/clarencenguyen/liftr/LiftrApplication.java
@@ -1,6 +1,7 @@
 package com.example.clarencenguyen.liftr;
 
 import android.app.Application;
+import android.os.Bundle;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
@@ -8,8 +9,10 @@ import com.parse.ParseACL;
  * Created by Phi on 10/3/2015.
  */
 public class LiftrApplication extends Application{
+
     @Override
-    public void onCreate{
+    public void onCreate (){
+        super.onCreate();
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "kxRXVjgUrcj9VERoLE1IUxBloZmRKLjCrknCuKzt", "3q2uX3teIqVRYrGlzLfvNkmBxTMnv7LwtBiByIu3");
